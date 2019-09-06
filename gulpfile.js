c600d055f1ec4b9a66cc54c7c63c5ed6f752c1d4
nikita <zotiken@gmail.com>
2019-09-06 16:24:32 +0300

    retina5

--- gulpfile.js
+++ gulpfile.js
@@ -7,31 +7,48 @@ var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var imagemin = require("gulp-imagemin");
+var webp = require("imagemin-webp");
+var extReplace = require("gulp-ext-replace");
 
-gulp.task("css", function () {
-  return gulp.src("source/less/style.less")
-    .pipe(plumber())
-    .pipe(sourcemap.init())
-    .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
+gulp.task("css", function() {
+    return gulp.src("source/less/style.less")
+        .pipe(plumber())
+        .pipe(sourcemap.init())
+        .pipe(less())
+        .pipe(postcss([
+            autoprefixer()
+        ]))
+        .pipe(sourcemap.write("."))
+        .pipe(gulp.dest("source/css"))
+        .pipe(server.stream());
 });
 
-gulp.task("server", function () {
-  server.init({
-    server: "source/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
+gulp.task("exportWebP", function() {
+    let src = "source/img/**/*.+(jpg|png)"; // Where your PNGs are coming from.
+    let dest = "source/img"; // Where your WebPs are going.
 
-  gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+    return gulp.src(src)
+        .pipe(imagemin([
+            webp({
+                quality: 75
+            })
+        ]))
+        .pipe(extReplace(".webp"))
+        .pipe(gulp.dest(dest));
+});
+
+gulp.task("server", function() {
+    server.init({
+        server: "source/",
+        notify: false,
+        open: true,
+        cors: true,
+        ui: false
+    });
+
+    gulp.watch("source/less/**/*.less", gulp.series("css"));
+    gulp.watch("source/*.html").on("change", server.reload);
 });
 
 gulp.task("start", gulp.series("css", "server"));
